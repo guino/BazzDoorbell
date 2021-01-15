d7ab0f63b1d893e0f3ec93824e420f4e0c04554c
wagner.oliveira <wagner.oliveira@fireking.com>
2021-01-14 23:57:11 -0500

    Fixed snap/mjpeg image glitches

---
+++ jpeg/jpeg.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int fd;							// File descriptor
+char *buff;						// jpeg buffer
+
+char *snaphdr = "Content-type: image/jpeg\r\n\r\n";
+
+char *mjpeghdr = "Cache-Control: no-cache\r\nConnection: Keep-Alive\r\nContent-Type: multipart/x-mixed-replace;boundary=------guinomjpegboundary\r\n\r\n";
+char *mjpegsep = "------guinomjpegboundary\r\nContent-Type: image/jpeg\r\nFile-Name: picture\r\nContent-Length: ";
+char mjpeglen[12];
+
+int main( int ac, char *av[] )
+{
+	// If not enough arguments, show usage and leave
+	if(ac < 4) {
+		fprintf(stderr, "usage: %s /proc/NNNN/mem address length [mjpeg]\n", av[0]);
+		exit(1);
+	}
+
+	// Open memory of process
+	if((fd = open(av[1], O_RDONLY)) < 0) {
+		fprintf(stderr, "Can't access %s\n", av[1]);
+		perror(":");
+		exit(1);
+	}
+
+	// Flag for mjpeg
+	int mjpeg = 0;
+	if(ac == 5 && strcmp(av[4], "mjpeg")==0)
+		mjpeg = 1;
+
+	// Set address and length of buffer
+	ulong jpegptraddr = strtoul(av[2], 0, 16);
+	ulong jpegaddr = 0;
+	ulong len = strtoul(av[3], 0, 0);
+	sprintf(mjpeglen, "%d\r\n\r\n", len);
+
+	// Read print jpeg address
+	lseek(fd, jpegptraddr, SEEK_SET);
+	read(fd, &jpegaddr, 4);
+	fprintf(stderr, "jpegptraddr=%x jpegaddr=%x\n", jpegptraddr, jpegaddr);
+
+	// Create buffers
+	buff = malloc(len*2);
+	if(!buff) {
+		fprintf(stderr, "Could not allocate buffers.\n");
+		exit(1);
+	}
+
+	// Initial read pointer position
+	char *ptr = buff;
+
+	// If mjpeg, send out initial header
+	if(mjpeg)
+		write(STDOUT_FILENO, mjpeghdr, strlen(mjpeghdr));
+
+	// Stdout write length
+	int outlen = 0;
+
+	do {
+		// While buffers are different (i.e. update in progress)
+		int match = 0;
+		while(match==0) {
+			// Copy data to current read pointer
+			lseek(fd, jpegaddr, SEEK_SET);
+			if(read(fd, ptr, len)<len) {
+				fprintf(stderr, "Could not read whole buffer\n");
+				break;
+			}
+
+			// Set next read pointer
+			if(ptr==buff)
+				ptr=buff+len;
+			else
+				ptr=buff;
+
+			// If buffers are the same
+			if(memcmp(buff, buff+len, len)==0)
+				match=1;
+
+			// Not too fast (10ms beteen checks)
+			usleep(10000);
+		}
+
+		// If out buffers match, send out data
+		if(match) {
+			// Header based on type
+			if(mjpeg) {
+				write(STDOUT_FILENO, mjpegsep, strlen(mjpegsep));
+				write(STDOUT_FILENO, mjpeglen, strlen(mjpeglen));
+			} else {
+				write(STDOUT_FILENO, snaphdr, strlen(snaphdr));
+			}
+			// JPEG buffer
+			outlen = write(STDOUT_FILENO, buff, len);
+		}
+
+		// Prevents re-run of snap/mjpeg too fast
+		usleep(100000);
+
+	} while(mjpeg==1 && outlen==len);
+
+	fprintf(stderr, "Stdout closed\n");
+	free(buff);
+
+	return 0;
+}
--- mmc/cgi-bin/snap.cgi
+++ mmc/cgi-bin/snap.cgi
@@ -1,10 +1,6 @@
 #!/bin/sh
-echo -e "Content-type: image/jpeg\r"
-echo -e "\r"
-# Delay so my apps don't reload too fast
-/mnt/mmc01/busybox usleep 300000
+# JPEG address (only enter the digits after 0x)
+ADDRESS=42ac2c
 # Get ppsapp PID
 PPSID=$(ps | grep -v grep | grep ppsapp | awk '{print $1}')
-# Get JPEG address
-JPEGADDR=$(/mnt/mmc01/busybox dd if=/proc/$PPSID/mem bs=1 skip=$((0x42ac2c)) count=4| /mnt/mmc01/busybox od -t x4 | /mnt/mmc01/busybox head -1 | awk '{print $2}')
-/mnt/mmc01/busybox dd if=/proc/$PPSID/mem bs=4096 skip=$((0x$JPEGADDR/4096)) count=14 | /mnt/mmc01/busybox dd bs=8 skip=1
+/mnt/mmc01/jpeg-arm /proc/$PPSID/mem $ADDRESS 57336 2> /dev/null
