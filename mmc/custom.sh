a4e2c245431fb537f84e995d9504d39bbd2d49b7
wagner.oliveira <wagner.oliveira@fireking.com>
2021-01-27 12:35:32 -0500

    Added set script and daily run to cleanup.cgi

--- mmc/custom.sh
+++ mmc/custom.sh
@@ -7,6 +7,13 @@ if [ ! -e /tmp/customrun ]; then
  if [ -e /mnt/mmc01/ppsapp ]; then
   PPSID=$(ps | grep -v grep | grep ppsapp | awk '{print $1}')
   kill $PPSID
+  #/mnt/mmc01/set record_enable 0
+  #/mnt/mmc01/set enable_event_record 1
   /mnt/mmc01/ppsapp &
  fi
+fi
+if [ ! -e /tmp/cleanup`date +%Y%m%d` ]; then
+ rm -rf /tmp/cleanup*
+ touch /tmp/cleanup`date +%Y%m%d`
+ /mnt/mmc01/cgi-bin/cleanup.cgi > /tmp/cleanup.log
 fi
